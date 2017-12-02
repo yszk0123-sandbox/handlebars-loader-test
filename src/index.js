405152b0e5d328e3b0c126e8c9b9f7ec9c5406d4
Yuji Suzuki <y0suzuki0123@gmail.com>
2017-12-03 01:08:42 +0900

    Add an example of custom helpers

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-const greeting = require('./greeting.hbs');
+const Handlebars = require('handlebars/runtime');
+const greeting = require('./templates/greeting.hbs');
+const format = require('./helpers/format');
+
+// HACK: Following code is not working.
+// "Hello, {{../helpers/format name}}" in greeting.hbs
+Handlebars.helpers.format = format;
 
 document.getElementById('root').innerHTML = greeting({ name: 'world' });
--- webpack.config.js
+++ webpack.config.js
@@ -34,7 +34,9 @@ module.exports = {
     rules: [
       {
         test: /\.hbs/,
-        use: 'handlebars-loader',
+        use: {
+          loader: 'handlebars-loader',
+        },
       },
     ],
   },
